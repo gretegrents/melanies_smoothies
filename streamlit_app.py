89b340466e0a30ebac7809d81ab4fb304c5051b6
Grete <gretegrents@gmail.com>
2026-09-12 19:38:53 +0200

    Add nutrition information retrieval for selected fruits
    Fetch nutrition information for each chosen fruit from the API and display it in the app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 
 
 # Write directly to the app
@@ -22,6 +21,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen) 
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = "insert into smoothies.public.orders(ingredients, name_on_order) values ('" + ingredients_string + "','" + name_on_order + "')"
